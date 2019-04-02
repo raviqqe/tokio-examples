fc140f01de623128de3f32c223328ddcfe6b68e0
Yota Toyama <raviqqe@gmail.com>
2019-04-02 15:44:24 +0000

    Run future

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#![feature(async_await, await_macro, futures_api)]
+
+extern crate tokio;
+
+use tokio::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    future_in_thread_in_future();
+}
+
+// success
+#[allow(unused)]
+fn future_in_thread() {
+    let handle = std::thread::spawn(|| {
+        tokio::run(future::lazy(move || {
+            tokio::spawn_async(async move {});
+            println!("Hello, world!");
+            Ok(())
+        }));
+    });
+
+    handle.join().unwrap();
+}
+
+// panic
+#[allow(unused)]
+fn future_in_thread_in_future() {
+    tokio::run(future::lazy(move || {
+        let handle = std::thread::spawn(|| {
+            tokio::spawn_async(async move {});
+            println!("Hello, world!");
+        });
+
+        handle.join().unwrap();
+        Ok(())
+    }));
 }
